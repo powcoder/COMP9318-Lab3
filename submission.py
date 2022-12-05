76c5555248315544f014c228939bf75cb3825ec5
powcoder <powcoder@163.com>
2022-12-05 10:25:31 +0800

    COMP9318 Lab3

---
+++ submission.py
@@ -0,0 +1,14 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+## import modules here 
+
+################# Question 1 #################
+
+def multinomial_nb(training_data, sms):# do not change the heading of the function
+    pass # **replace** this line with your code
